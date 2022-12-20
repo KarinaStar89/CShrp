fbb8e83f8336535a41132ce7a5b5f10b80580735
Karina Ibragimova <karina.nebo77@mail.ru>
2022-12-20 23:03:59 +0500

    Add Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string evenNamber = "";
+int i = 1;
+while (i <= n)
+{
+    if (i % 2 == 0)
+    {
+        if (evenNamber == "")
+        {
+            evenNamber = $"{i}";
+        }
+        else
+        {
+            evenNamber = $"{evenNamber}, {i}";
+        }
+    }
+    i++;
+}
+Console.WriteLine($"До числа {n} четные числа: {evenNamber}");
