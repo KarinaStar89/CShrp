4f9d9037b1b8a6798bdf1e9bd61ba061e378a961
Karina Ibragimova <karina.nebo77@mail.ru>
2023-01-15 19:18:19 +0500

    Add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿
+var a = GetNumber("Введите число");
+var b = GetNumber("Введите степень числа");
+
+var result = System.Math.Pow(a, b);
+
+Console.WriteLine($"Число {a} в степени {b} равно {result}");
+
+double GetNumber(string text)
+{
+    Console.WriteLine(text);
+    double number = Convert.ToInt32(Console.ReadLine());
+    while(number < 1)
+    {
+        Console.WriteLine($"Введите положительное целое число");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    return number;
+}
